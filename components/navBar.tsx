a444342b0b68c5d597fb1a37a24966b125a50f5f
LeRoiAdib <dev@romain-laurent.fr>
2024-09-01 17:29:55 +0200

    update the nav bar active style

--- components/navBar.tsx
+++ components/navBar.tsx
@@ -4,15 +4,10 @@ import Link from 'next/link'
 import { usePathname } from 'next/navigation'
 import { motion } from 'framer-motion'
 import useScrollDirection from '../hooks/useScrollDirection'
-import { useEffect } from 'react'
 
 export default function NavBar() {
     const pathname = usePathname()
     const { scrollDirection } = useScrollDirection()
-
-    useEffect(() => {
-        console.log(scrollDirection)
-    }, [scrollDirection])
 
     return (
         <motion.nav
@@ -24,13 +19,13 @@ export default function NavBar() {
                 <h1>ROMAIN LAURENT</h1>
             </Link>
             <div className='flex gap-x-8'>
-                <Link className={`link ${pathname === '/projects' ? 'text-red-700' : ''}`} href='/projects'>
+                <Link className={`link ${pathname === '/projects' ? 'active' : ''}`} href='/projects'>
                     Projects
                 </Link>
-                <Link className={`link ${pathname === '/background' ? 'text-red-700' : ''}`} href='/background'>
+                <Link className={`link ${pathname === '/background' ? 'active' : ''}`} href='/background'>
                     Background
                 </Link>
-                <Link className={`link ${pathname === '/contact' ? 'text-red-700' : ''}`} href='/contact'>
+                <Link className={`link ${pathname === '/contact' ? 'active' : ''}`} href='/contact'>
                     Contact
                 </Link>
             </div>
