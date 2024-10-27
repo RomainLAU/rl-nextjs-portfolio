dcf9133adc10467951a2190e7fbd7ef2a923d87c
LeRoiAdib <dev@romain-laurent.fr>
2024-10-27 14:40:14 +0100

    refacto: update light mode and update formation style

--- components/experienceCard.tsx
+++ components/experienceCard.tsx
@@ -43,37 +43,37 @@ export default function ExperienceCard({ element }: { element: Experience }) {
                 <motion.h2
                     {...getAnimationProps(1)}
                     className='font-bold text-[3em] md:text-[16em] max-w-min'
-                    style={{ wordSpacing: isMobile ? '' : '440px', lineHeight: isMobile ? '' : '30vh' }}>
+                    style={{ wordSpacing: isMobile ? '' : '440px', lineHeight: isMobile ? '' : '30dvh' }}>
                     {experience.title}
                 </motion.h2>
                 <div className='md:h-full w-full md:w-max flex flex-col gap-y-8 justify-center'>
-                    <motion.p {...getAnimationProps(2)} className='text-[3em] md:text-[calc(10vh+12em)] font-extrabold font-[ui-monospace] leading-[normal]'>
+                    <motion.p {...getAnimationProps(2)} className='text-[3em] md:text-[calc(10dvh+12em)] font-extrabold font-[ui-monospace] leading-[normal]'>
                         {new Date(experience.started_at).toLocaleDateString(locale).replaceAll('-', '/')}
                     </motion.p>
                     <div className='flex flex-col w-full gap-y-4'>
                         <motion.p {...getAnimationProps(3)} className='text-xl md:text-4xl text-left self-start'>
                             {experience.company} - {experience.contract}
                         </motion.p>
-                        <motion.div className='text-xl md:text-2xl md:text-right flex-wrap flex md:w-full md:flex-nowrap gap-x-4 md:self-end'>
+                        <motion.div className='text-xl md:text-2xl md:text-right flex-wrap flex w-full md:w-auto md:flex-nowrap gap-x-4 md:self-end'>
                             {experience.skills &&
                                 experience.skills.map((skill, index) => (
                                     <motion.p
                                         key={`skill-${skill.id}-${experience.title}`}
                                         initial={{ opacity: 0, y: 10 }}
-                                        animate={{ opacity: isInView ? 1 : 0, y: isInView ? 0 : 10 }}
+                                        animate={{ opacity: isInView || !isMobile ? 1 : 0, y: isInView || !isMobile ? 0 : 10 }}
                                         transition={{ duration: 0.3, delay: isInView ? 1.3 + index * 0.1 : 0 }}>
                                         {skill.title}
                                     </motion.p>
                                 ))}
                         </motion.div>
                     </div>
-                    <motion.p {...getAnimationProps(5.5)} className='text-[3em] md:text-[calc(10vh+10em)] leading-[normal] font-extrabold font-[ui-monospace]'>
+                    <motion.p {...getAnimationProps(5.5)} className='text-[3em] md:text-[calc(10dvh+12em)] leading-[normal] font-extrabold font-[ui-monospace]'>
                         {(experience.finished_at && new Date(experience.finished_at).toLocaleDateString(locale).replaceAll('-', '/')) ?? 'Present'}
                     </motion.p>
                 </div>
                 <motion.p
                     {...getAnimationProps(7)}
-                    className='w-full max-w-screen md:w-[32rem] text:xl md:text-[clamp(12px,calc(15%+1.5em),100px)] whitespace-break-spaces'
+                    className='w-full max-w-screen md:w-[32rem] text-xl md:text-[clamp(12px,calc(15%+1.5em),100px)] whitespace-break-spaces'
                     style={{ marginTop: experience.id % 2 === 0 ? 'auto' : '0', marginBottom: !(experience.id % 2 === 0) ? 'auto' : '0' }}>
                     {experience.description}
                 </motion.p>
--- components/formationCard.tsx
+++ components/formationCard.tsx
@@ -23,41 +23,41 @@ export default function FormationCard({ element }: { element: Formation }) {
             <motion.div
                 ref={ref}
                 {...getAnimationProps()}
-                className='w-full md:min-w-max md:h-[calc(100vh - 8rem)] shadow-md rounded-lg text-white flex flex-col md:flex-row md:items-center gap-8 md:gap-96'>
+                className='w-full md:min-w-max md:h-[calc(100dvh-12rem)] shadow-md rounded-lg text-white flex flex-col md:flex-row md:items-center gap-8 md:gap-96'>
                 <motion.h2
                     {...getAnimationProps(1)}
                     className='font-bold text-[3em] md:text-[16em] max-w-min'
-                    style={{ wordSpacing: isMobile ? '' : '440px', lineHeight: isMobile ? '' : '30vh' }}>
+                    style={{ wordSpacing: isMobile ? '' : '440px', lineHeight: isMobile ? '' : '30dvh' }}>
                     {formation.school}
                 </motion.h2>
                 <div className='md:h-full w-full md:w-max flex flex-col gap-y-8 justify-center'>
-                    <motion.p {...getAnimationProps(2)} className='text-[3em] md:text-[22em] font-extrabold font-[ui-monospace]'>
+                    <motion.p {...getAnimationProps(2)} className='text-[3em] md:text-[calc(10dvh+12em)] font-extrabold font-[ui-monospace]'>
                         {new Date(formation.started_at).toLocaleDateString(locale).replaceAll('-', '/')}
                     </motion.p>
                     <div className='flex flex-col w-full gap-y-4'>
                         <motion.p {...getAnimationProps(3)} className='text-2xl md:text-4xl text-left self-start'>
                             {formation.title}
                         </motion.p>
-                        <motion.div className='w-full text-xl md:text-2xl text-right flex flex-wrap md:flex-nowrap gap-x-4 md:self-end'>
+                        <motion.div className='text-xl md:text-2xl md:text-right flex-wrap flex w-full md:w-auto md:flex-nowrap gap-x-4 md:self-end'>
                             {formation.skills &&
                                 formation.skills.map((skill, index) => (
                                     <motion.p
                                         key={`skill-${skill.id}-${formation.title}`}
                                         initial={{ opacity: 0, y: 10 }}
-                                        animate={{ opacity: isInView ? 1 : 0, y: isInView ? 0 : 10 }}
-                                        transition={{ duration: 0.5, delay: isInView ? 1.3 + index * 0.1 : 0 }}>
+                                        animate={{ opacity: isInView || !isMobile ? 1 : 0, y: isInView || !isMobile ? 0 : 10 }}
+                                        transition={{ duration: 0.3, delay: isInView ? 1.3 + index * 0.1 : 0 }}>
                                         {skill.title}
                                     </motion.p>
                                 ))}
                         </motion.div>
                     </div>
-                    <motion.p {...getAnimationProps(5.5)} className='text-[3em] md:text-[22em] font-extrabold font-[ui-monospace]'>
+                    <motion.p {...getAnimationProps(5.5)} className='text-[3em] md:text-[calc(10dvh+12em)] font-extrabold font-[ui-monospace]'>
                         {(formation.finished_at && new Date(formation.finished_at).toLocaleDateString(locale).replaceAll('-', '/')) ?? 'Present'}
                     </motion.p>
                 </div>
                 <motion.p
                     {...getAnimationProps(7)}
-                    className='w-full md:w-[32rem] text-xl md:text-[2rem] whitespace-break-spaces'
+                    className='w-full max-w-screen md:w-[32rem] text-xl md:text-[clamp(12px,calc(15%+1.5em),100px)] whitespace-break-spaces'
                     style={{ marginTop: formation.id % 2 === 0 ? 'auto' : '0', marginBottom: !(formation.id % 2 === 0) ? 'auto' : '0' }}>
                     {formation.description}
                 </motion.p>
