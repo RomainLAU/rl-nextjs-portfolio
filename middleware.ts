da3326f3510e27f8dab6a1853eb9ae84da9ec150
LeRoiAdib <dev@romain-laurent.fr>
2024-10-27 03:18:53 +0100

    fix: attempt at fixing middlware again

--- middleware.ts
+++ middleware.ts
@@ -4,7 +4,7 @@ type Environment = 'production' | 'development' | 'other'
 export function middleware(req: NextRequest, ev: NextFetchEvent) {
     const currentEnv = process.env.NODE_ENV as Environment
 
-    if (currentEnv === 'production' && req.headers.get('x-forwarded-proto')?.indexOf('https') !== -1) {
+    if (currentEnv === 'production' && req.headers.get('x-forwarded-proto') !== 'https') {
         return NextResponse.redirect(`https://${req.headers.get('host')}${req.nextUrl.pathname}`, 301)
     }
     return NextResponse.next()
