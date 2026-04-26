394925cbb8cddd9c7b64839553c95299ccfa1766
LeRoiAdib <dev@romain-laurent.fr>
2026-04-26 11:18:41 -0400

    chore: remove html-react-parser dependency and unused localhost/api image patterns

--- next.config.mjs
+++ next.config.mjs
@@ -6,18 +6,6 @@ const nextConfig = {
     images: {
         remotePatterns: [
             {
-                protocol: 'http',
-                hostname: 'localhost',
-                port: '1337',
-                pathname: '/uploads/**',
-            },
-            {
-                protocol: 'https',
-                hostname: 'api.romain-laurent.fr',
-                port: '',
-                pathname: '/uploads/**',
-            },
-            {
                 protocol: 'https',
                 hostname: 'res.cloudinary.com',
                 port: '',
